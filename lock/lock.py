25a9d06399b3150b451f06fe01a9ab1cfd978277
饶驹 <raoju1580@qq.com>
2023-08-07 14:33:48 +0800

    feature(lock): 多线程安全，锁

---
+++ lock/lock.py
@@ -0,0 +1,25 @@
+import threading
+
+lock = threading.Lock()
+class Account:
+    def __init__(self, balance):
+        self.balance = balance
+
+
+def draw(account, amount):
+    with lock:
+        if (account.balance >= amount):
+            print(threading.currentThread().name, '取成功')
+            account.balance -= amount
+            print(threading.currentThread().name, '余额', account.balance)
+        else:
+            print(threading.currentThread().name, '取失败，余额不足')
+
+
+if __name__ == '__main__':
+    account = Account(1000)
+    ta = threading.Thread(target=draw, args=(account, 800))
+    tb = threading.Thread(target=draw, args=(account, 800))
+
+    ta.start()
+    tb.start()
