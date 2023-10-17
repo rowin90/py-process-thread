204833097737ec0572f880b0b678ce3d3b69e0de
饶驹 <raoju1580@qq.com>
2023-10-17 13:45:41 +0800

    feature(lock): 手动线程锁，释放锁

--- lock/lock.py
+++ lock/lock.py
@@ -7,6 +7,12 @@ class Account:
 
 
 def drawWithLock(account, amount):
+    """
+    自动上下文管理锁
+    :param account:
+    :param amount:
+    :return:
+    """
     with lock:
         if (account.balance >= amount):
             print(threading.currentThread().name, '取成功')
@@ -14,6 +20,22 @@ def drawWithLock(account, amount):
             print(threading.currentThread().name, '余额', account.balance)
         else:
             print(threading.currentThread().name, '取失败，余额不足')
+def drawWithLockManual(account, amount):
+    """
+    手动加锁，释放锁
+    :param account:
+    :param amount:
+    :return:
+    """
+    lock.acquire() # 加锁
+    if (account.balance >= amount):
+        print(threading.currentThread().name, '取成功')
+        account.balance -= amount
+        print(threading.currentThread().name, '余额', account.balance)
+    else:
+        print(threading.currentThread().name, '取失败，余额不足')
+
+    lock.release()
 def draw(account, amount):
     if (account.balance >= amount):
         print(threading.currentThread().name, '取成功')
@@ -25,8 +47,10 @@ def draw(account, amount):
 
 if __name__ == '__main__':
     account = Account(1000)
-    ta = threading.Thread(target=draw, args=(account, 800))
-    tb = threading.Thread(target=draw, args=(account, 800))
+    # ta = threading.Thread(target=draw, args=(account, 800))
+    # tb = threading.Thread(target=draw, args=(account, 800))
+    ta = threading.Thread(target=drawWithLockManual, args=(account, 800))
+    tb = threading.Thread(target=drawWithLockManual, args=(account, 800))
 
     ta.start()
     tb.start()
